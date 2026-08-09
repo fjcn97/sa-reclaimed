49c390934dd84a1403806fad1f61b89bb7692256
Fabio Nogueira <fabionog97@gmail.com>
2026-08-09 05:17:29 +0100

    refactor: Delete unnecessary files
    Remove smoke test scripts for various gameplay features including Super Sonic, terrain surfaces, time attack lobby/results, time records, Tiny Chao, title flow, touch controls, turnaround bars, UI source coverage, whirlwinds, windup sticks, and Yado enemies

--- godot-project/path_probe.gd
+++
@@ -1,7 +0,0 @@
-extends SceneTree
-
-func _init() -> void:
-	var path := ProjectSettings.globalize_path("res://../data/sa2/maps/zone_1/act_1/entities/rings.csv")
-	print(FileAccess.file_exists(path))
-	print(path)
-	quit()
